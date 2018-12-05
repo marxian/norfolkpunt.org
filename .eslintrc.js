6b3bbe6ff444175ac22405ee1dfebace991fc07a
Rupert Redington <rupert@neontribe.co.uk>
2018-12-05 21:54:37 +0000

    fix: update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 /* eslint-env node */
 module.exports = {
-  extends: ['react'],
-  rules: {
-    semi: 0,
-    'comma-dangle': ['error', 'always-multiline'],
-    indent: ['error', 2],
-    'react/prop-types': 0,
-    'prefer-reflect': 0,
-    'space-before-function-paren': 0,
-  },
+  extends: ['react-app'],
+  // rules: {
+  //   semi: 0,
+  //   'comma-dangle': ['error', 'always-multiline'],
+  //   indent: ['error', 2],
+  //   'react/prop-types': 0,
+  //   'prefer-reflect': 0,
+  //   'space-before-function-paren': 0,
+  // },
   globals: {
     graphql: false,
   },
