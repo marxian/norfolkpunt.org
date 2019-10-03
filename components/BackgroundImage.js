ee9a478409f1896909feda85e6c4f65c006bbe52
Rupert Redington <rupert@neontribe.co.uk>
2019-10-03 11:21:51 +0100

    fix: layout

--- components/BackgroundImage.js
+++ components/BackgroundImage.js
@@ -2,16 +2,17 @@ import React from 'react'
 import GatsbyBackgroundImage from 'gatsby-background-image'
 
 export default ({ data, className, children }) => {
+  let fluid = {
+    aspectRatio: 1,
+    sizes: '(max-width: 300px) 100vw, 300px',
+    src: data.fluid.src,
+    srcSet: data.fluid.srcSet,
+    base64: data.lqip.preSrc,
+  }
   return (
     <GatsbyBackgroundImage
       className={className}
-      fluid={{
-        aspectRatio: 1,
-        sizes: '(max-width: 300px) 100vw, 300px',
-        src: data.fluid.src,
-        srcSet: data.fluid.srcSet,
-        base64: data.lqip,
-      }}
+      fluid={fluid}
       backgroundColor={true}
     >
       {children}
--- components/Image.js
+++ components/Image.js
@@ -2,17 +2,15 @@ import React from 'react'
 import GatsbyImage from 'gatsby-image'
 
 export default ({ data, className }) => {
+  let fluid = {
+    aspectRatio: 1,
+    sizes: '(max-width: 300px) 100vw, 300px',
+    src: data.fluid.src,
+    srcSet: data.fluid.srcSet,
+    base64: data.lqip.preSrc,
+  }
+
   return (
-    <GatsbyImage
-      className={className}
-      fluid={{
-        aspectRatio: 1,
-        sizes: '(max-width: 300px) 100vw, 300px',
-        src: data.fluid.src,
-        srcSet: data.fluid.srcSet,
-        base64: data.lqip,
-      }}
-      backgroundColor={true}
-    />
+    <GatsbyImage className={className} fluid={fluid} backgroundColor={true} />
   )
 }
