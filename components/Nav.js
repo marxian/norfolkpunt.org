adbc0e515127f0ff9119cbf908448da284c02c8d
Rupert Redington <rupert@neontribe.co.uk>
2019-10-03 16:23:00 +0100

    feat: technicalities

--- components/Nav.js
+++ components/Nav.js
@@ -2,25 +2,27 @@ import React from 'react'
 import Link from 'next/link'
 
 export default () => (
-  <nav className="bg-black-90 w-100 pv1 ph3">
-    <Link href="/">
-      <a className="link dim tc" title="Home">
-        <h1>
-          <span className="f2 white">Norfolk&nbsp;Punt</span>{' '}
-          <span className="f3 moon-gray">Owners&nbsp;Association</span>
-        </h1>
-      </a>
-    </Link>
-    {/* <div className="db dtc-l v-mid w-100 w-50-ns tc tr-l">
-      <Link href="/boats">
-        <a className="link dim white dib f4 f3-l dib mr3 mr4-l">Boats</a>
+  <nav className="bg-black-90 w-100 pa1 flex flex-column flex-row-l">
+    <div className="tc w-50-l">
+      <Link href="/">
+        <a className="link dim" title="Home">
+          <h1>
+            <span className="f2 white">Norfolk&nbsp;Punt</span>{' '}
+            <span className="f3 moon-gray">Owners&nbsp;Association</span>
+          </h1>
+        </a>
       </Link>
-      <Link href="/technical">
-        <a className="link dim white dib f4 f3-l dib mr3 mr4-l">Technical</a>
+    </div>
+    <div className="w-100 w-50-l tc tr-l mv4-l f4 b">
+      {/* <Link href="/boats">
+        <a className="link dim white ph1">Boats</a>
       </Link>
-      <Link href="/contact">
-        <a className="link dim white dib f4 f3-l dib mr3 mr4-l">Contact</a>
+      <Link href="/gallery">
+        <a className="link dim white ph1">Gallery</a>
+      </Link> */}
+      <Link href="/technicalities">
+        <a className="link dim white underline-hover ph1">Technicalities</a>
       </Link>
-    </div> */}
+    </div>
   </nav>
 )
