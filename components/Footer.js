89f7a278f9d420fb4e21733a0cc40d9b332a42ed
Rupert Redington <rupert@neontribe.co.uk>
2019-10-05 15:17:04 +0100

    feat: improved footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,16 +1,23 @@
 import React from 'react'
 
 export default () => (
-  <footer className="bg-black-90 white pv3 pv4-l ph4">
-    <p className="f6">
-      <span className="dib mr4 mr5-ns">
-        ©2019 Norfolk&nbsp;Punt Owners&nbsp;Association
-      </span>
+  <footer className="cf bg-black-90 white f6 pv3 pv4-l ph4">
+    <p className="fr f4">
       <a
         className="link white underline-hover"
         href="mailto:norfolkpuntsec@yahoo.com"
       >
         Contact
+      </a>
+    </p>
+    <p>&copy;2019 Norfolk&nbsp;Punt Owners&nbsp;Association</p>
+    <p>
+      Most photographs by kind permission &copy;&nbsp;
+      <a
+        className="link white underline-hover"
+        href="https://www.myerscoughpictures.org.uk"
+      >
+        Robin&nbsp;Myerscough
       </a>
     </p>
   </footer>
