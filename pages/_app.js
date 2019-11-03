087735add739fc3a4398b665baa9c322df0ba8be
Rupert Redington <rupert@neontribe.co.uk>
2019-11-03 18:35:21 +0000

    feat: favico

--- pages/_app.js
+++ pages/_app.js
@@ -18,6 +18,24 @@ class MyApp extends App {
             name="Description"
             content="The Norfolk Punt is a performance racing dinghy with a long and fascinating history."
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
         </Head>
         <Nav />
         <main>
