0b3d2cff514f64c56c7f5333a23393dd24586708
Rupert Redington <rupert@neontribe.co.uk>
2020-01-14 20:24:47 +0000

    feat: show calendar embed

--- pages/technicalities.js
+++ pages/technicalities.js
@@ -31,6 +31,20 @@ export default () => (
     </article>
     <article className="bt bb b--black-10">
       <h2>
+        <a name="calendar">Events Calendar</a>
+      </h2>
+      <iframe
+        title="Calendar"
+        src="https://calendar.google.com/calendar/b/3/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Europe%2FLondon&amp;src=bm9yZm9sa3B1bnRAZ21haWwuY29t&amp;color=%2322AA99&amp;showDate=1&amp;showNav=1&amp;showTitle=0&amp;showCalendars=0&amp;hl=en_GB&amp;showTabs=0&amp;showPrint=0&amp;showTz=0"
+        style={{ 'border-width': 0 }}
+        width="100%"
+        height="600"
+        frameborder="0"
+        scrolling="no"
+      ></iframe>
+    </article>
+    <article className="bt bb b--black-10">
+      <h2>
         <a name="class-register">Class Register</a>
       </h2>
       <ul>
